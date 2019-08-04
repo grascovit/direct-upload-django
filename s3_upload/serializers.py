13fdf0a010a5fe682772bdc6bbad2cffbb9778e3
Gabriel Rascovit <rascovit@gmail.com>
2019-08-04 14:45:37 -0300

    create view to generate presigned post url and document viewset

---
+++ s3_upload/serializers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rest_framework import serializers
+from .models import Document
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = ['id', 'attachment']
--- s3_upload/urls.py
+++ s3_upload/urls.py
@@ -1,6 +1,15 @@
-from django.conf.urls import url
-from .views import Home
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import Home, GeneratePresignedS3Url, DocumentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'documents', DocumentViewSet)
 
 urlpatterns = [
-	url(r'^home/$', Home.as_view())
+	url(r'^', include(router.urls)),
+	url(r'^home/$', Home.as_view(), name='home'),
+	url(r'^generate_presigned_s3_url/$', GeneratePresignedS3Url.as_view(), name='generate_presigned_s3_url'),
 ]
